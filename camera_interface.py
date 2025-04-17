36ae103fa54b252a87754aaed5f792d899f24f47
kenembanisi <kene.mbanisi@gmail.com>
2025-04-17 05:27:33 -0400

    add lesson2 with updates and dependencies

---
+++ camera_interface.py
@@ -0,0 +1,26 @@
+""" 
+Simple script to display and write frames from the selected camera using OpenCV
+"""
+import cv2
+
+video_id = 6 # this is setup-dependent and would need to change. Ranges from 0-10+
+cap = cv2.VideoCapture(video_id) 
+i = 0
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow("frame", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            print("Saving to frame.png")
+            frame_str = 'calibration_imgs/calibration_img'+str(i)+'.png'
+            # frame_str = 'aruco-board-cubes.png'
+            cv2.imwrite(frame_str, frame)
+            i+= 1
+
+    else:
+        print("Failed to capture frame")
+        break
+
+cap.release()
